4df3101e61150395a2d3d28ef3ce355c7bcc7a33
Brad <bcoffield@gmail.com>
2021-09-29 13:41:49 -0600

    FINALLY

--- web/src/assets/scripts/main.js
+++ web/src/assets/scripts/main.js
@@ -6,3 +6,12 @@ import './modules/nav'
 
 // Algolia
 import './modules/algoliaSearch'
+import lodashMethods from '../../data/lodashMethods'
+(async () => {
+    let lodashMethods = require('../../data/lodashMethods')
+    // console.log(await lodashMethods())
+    let lodashMethodss = await lodashMethods()
+    console.log(lodashMethodss);
+    let spot = document.getElementById('singleMethod')
+    spot.insertAdjacentHTML('afterbegin', `<h2>${lodashMethodss[0].method_name}</h2>`)
+})()
