89a9b85faadc8665cb521f8e8d7b5d8cf6e952cf
Brad Coffield <bcoffield@gmail.com>
2021-10-07 09:31:55 -0600

    to dataset changes to go along with new scraper

--- studio/write-to-dataset.js
+++ studio/write-to-dataset.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const sanityClient = require("@sanity/client");
 const seedrandom = require("seedrandom");
-// const ourLodashData = require("/home/brad/Dropbox/Web Design/1-Coding Projects/022-scraping-lodash/lodash-util-methods.json");
+const ourLodashData = require("/home/brad/Dropbox/Web Design/1-Coding Projects/022-scraping-lodash/lodash-methods.json");
 
 const apiToken = process.env.SANITY_TOKEN;
 const projectId = process.env.SANITY_PROJECT_ID;
@@ -20,26 +20,42 @@ const createIdBasedOnTitle = ourLodashData.map((i) => {
   return { ...i, id: rng() };
 });
 
+function sanitizeMethodName(i) {
+  let match = /^.*(?=\()/.exec(i.substring(2));
+  let result = ""
+  if (match && match[0]) {
+    console.log(match[0]);
+    result = match[0]
+  }
+  return result;
+}
+
 function transform(i) {
   return {
     _id: i.id.toString().substr(2),
     _type: "lodash_method",
-    method_name: i.title,
+    method: i.title,
+    methodName: sanitizeMethodName(i.title),
     links: i.links,
     description: i.description,
     arguments: i.arguments,
     returns: i.returns,
     example: i.example,
     category: i.category,
-    search_helpers: [],
+    search_helpers: [], 
+    featured_on: [],
+    related_methods: [],
+    lodash_version: i.lodashVersion,
   };
 }
+
+ 
 
 let readyToGo = createIdBasedOnTitle.map((item) => {
   return transform(item);
 });
 
-// console.log(readyToGo);
+console.log(readyToGo);
 
 let transaction = client.transaction();
 readyToGo.forEach((document) => {
